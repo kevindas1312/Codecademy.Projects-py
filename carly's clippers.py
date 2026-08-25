36a981c35bfe06c1bdcd1a465f9061c2e0df99c7
kevindas1312 <dusty131207@gmail.com>
2026-08-25 22:37:47 +0800

    Correct price calculation logic in carly's clippers.py
    Fix variable name and increment logic in price calculation.

--- carly's clippers.py
+++ carly's clippers.py
@@ -5,7 +5,7 @@ prices = [30, 25, 40, 20, 20, 35, 50, 35]
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
-for i in prices :
+for a in prices :
   total_price += i
   i+=1
 print("Total price : ", total_price)
